c96451be31d8b3851619b6453a1105c77de73349
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-02-10 08:11:23 +0530

    stuff

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-indigo-200 h-screen'>
+        <h1 className='text-black text-4xl'>TaskMaster</h1>
+    </div>
+  )
+}
+
+export default App
