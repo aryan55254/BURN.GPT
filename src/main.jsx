633106def535d1772489703783aefcc3d829c71f
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-02-16 19:47:36 +0530

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Counter from './Counter'
 import StopWatch from './StopWatch'
+import { BrowserRouter , Route ,Routes,Router } from 'react-router-dom'
+import Landing from './Landing'
+import Home from './Home'
+import Task from './Task'
+import Notes from './Notes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter />
-    <StopWatch />
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Landing />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/stopwatch' element={<StopWatch />} />
+      <Route path='/counter' element={<Counter />} />
+      <Route path='/tasks' element={<Task />} />
+      <Route path='/notes' element={<Notes />} />
+    </Routes>
+   </BrowserRouter>
   </StrictMode>,
 )
